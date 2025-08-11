8701d0061a5181a5d106f9cdbcc87557a26c5b44
Android Studio <devandroid@ort>
2025-08-11 09:57:38 -0300

    10)Sorpresa para el equipo

--- react-native-colab/screens/Home.js
+++ react-native-colab/screens/Home.js
@@ -1,14 +1,23 @@
-// screens/Home.js
+
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Linking } from 'react-native';
 import Counter from '../components/Counter';
 
 export default function Home({ navigation }) {
+  const abrirGoogle = () => {
+    Linking.openURL('https://www.google.com');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Pantalla de Inicio</Text>
       <Counter />
       <Button title="Ir a Perfiles" onPress={() => navigation.navigate('Perfiles')} />
+
+      {/* Botón sorpresa */}
+      <View style={{ marginTop: 20 }}>
+        <Button title="🔍 Abrir Google" onPress={abrirGoogle} color="#FF5733" />
+      </View>
     </View>
   );
 }
